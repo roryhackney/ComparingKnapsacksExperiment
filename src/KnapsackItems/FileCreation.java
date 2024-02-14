f74e9b80ef07eb7705d7f154da092e87d57e7dbe
Rory Hackney <roryhackney@gmail.com>
2024-02-13 18:04:43 -0800

    created extra input files and added provided ones

---
+++ src/KnapsackItems/FileCreation.java
@@ -0,0 +1,64 @@
+package KnapsackItems;
+
+import java.io.*;
+import java.util.Random;
+
+/**
+ * Creates 3 input files of size 20, 25, 30
+ * @author Rory Hackney
+ */
+public class FileCreation {
+    private static final int MAX = 201;
+    private static final int MIN = 1;
+
+    private FileCreation() {}
+
+    public static void main(String[] args) {
+        int knapsackNum = 4;
+        for (int cap = 20; cap <= 30; cap += 5) {
+            createFile(cap, knapsackNum++);
+        }
+    }
+
+    //create file 20: input4.csv, 4, 20 /n random ints between 1 and 200 /n same
+    //create file 25: input5.csv, 5, 25, /n 25 random ints, 25 random ints
+    //create file 30: input6.csv, 6, 30, /n 30 random ints, 30 random int
+
+    /**
+     * Creates a new Knapsack file named inputsX.csv with capacity random weights and benefits
+     * @param capacity the capacity of the Knapsack; must be at least 1 to proceed
+     * @param knapsackNum the number of the Knapsack, must be 4 or more (to avoid overwriting provided files) to proceed
+     * @return whether the file was successfully created or not
+     */
+    public static boolean createFile(int capacity, int knapsackNum) {
+        if (knapsackNum < 4 || capacity < 1) return false;
+        System.out.println();
+        File file = new File("src/KnapsackItems/inputs" + knapsackNum + ".txt");
+//        System.out.println("---" + file.getAbsolutePath());
+        try {
+            //delete if already exists
+            file.delete();
+            //so you can make a new one
+            file.createNewFile();
+            //write to file
+            PrintStream d = new PrintStream(file);
+            //knapsack number and capacity
+            d.print(knapsackNum);
+            d.print(',');
+            d.print(capacity);
+            d.print('\n');
+            //values line, then weights line
+            Random r = new Random();
+            for (int line = 0; line < 2; line++) {
+                d.print(r.nextInt(MIN, MAX));
+                for (int i = 2; i <= capacity; i++) {
+                    d.print(',');
+                    d.print(r.nextInt(MIN, MAX));
+                }
+                d.print('\n');
+            }
+            d.close();
+            return true;
+        } catch (Exception e) {return false;}
+    }
+}
