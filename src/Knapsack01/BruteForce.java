aeab26473276a8d138f0cc58cd1da08ee6030115
Kelvin R <kelvinrbNsc@gmail.com>
2024-02-16 13:01:50 -0800

    Saving progress for Greedy&Brute 01

--- src/Knapsack01/BruteForce.java
+++ src/Knapsack01/BruteForce.java
@@ -1,7 +1,12 @@
 package Knapsack01;
+import KnapsackItems.Item;
+import KnapsackItems.Knapsack;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Class for BruteForce on a KnapSack 01 Problem
+ * Class implementation for BruteForce on a KnapSack 01 Problem
+ *
  * @author KRB
  * @version 1.0
  */
@@ -9,8 +14,21 @@ public class BruteForce {
 
     public BruteForce(Knapsack knapsack){
         Item[] items = knapsack.getItems();
-        for(int index = 0; index < items.length; index++) {
-            items[index]
+        int cap = knapsack.getCapacity();
+
+        int currentLoad = 0;
+        int currentBenefit = 0;
+        List<Item> combo = new ArrayList<>();
+
+        for(int i = 0; i < items.length; i++) {
+            int currentItemWeight = items[i].getWeight();
+            if (currentLoad < cap && (currentLoad + currentItemWeight) <= cap) {
+                currentLoad += currentItemWeight;
+                combo.add(items[i]);
+                currentBenefit += items[i].getBenefit();
+            } else {
+                break;
+            }
         }
     }
 }
