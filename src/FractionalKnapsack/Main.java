74e115879af71daa26ace406d4d0909264c3bc2d
Rory Hackney <roryhackney@gmail.com>
2024-02-20 22:35:55 -0800

    javadoc

--- src/FractionalKnapsack/Main.java
+++ src/FractionalKnapsack/Main.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Knapsack bag = new Knapsack(1, 5, new Item[]{new Item(3, 3), new Item(2, 2), new Item(1, 1)});
-//        bag = KnapsackItems.FileReading.readFile("inputs.csv");
+        bag = KnapsackItems.FileReading.readFile("inputs.csv");
         BruteForce x = new BruteForce(bag);
         System.out.println(bag.getCapacity());
         System.out.println(Arrays.toString(bag.getItems()));
