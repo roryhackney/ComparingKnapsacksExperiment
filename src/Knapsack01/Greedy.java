7581cce7deb42401a190e48b057aa3c3306396fb
Kelvin R <kelvinrbNsc@gmail.com>
2024-02-20 22:55:16 -0800

    Broke up 01 Greedy into more methods

--- src/Knapsack01/Greedy.java
+++ src/Knapsack01/Greedy.java
@@ -12,47 +12,59 @@ import java.util.*;
  * @version 1.0
  */
 public class Greedy {
+    /** Private List of Items variable for the best possible combinations **/
+    private List<Item> bestCombo;
+    /** Private integer variable for the best possible benefits **/
+    private int bestBenefit;
     /**
      * Constructor for Greedy Algorithm
      * @param knapsack Desired Knapsack to run algorithm through
      */
     public Greedy(Knapsack knapsack) {
+        bestCombo = new ArrayList<>();
+        bestBenefit = 0;
         Item[] items = knapsack.getItems();
         int cap = knapsack.getCapacity();
-
         int totalLoad = 0;
-        int totalBenefit = 0;
-        List<Item> combo = new ArrayList<>();
 
-//        List<Double> ratios = new ArrayList<>();
-//        for (Item item : items) {
-//            double ratio = (double)item.getBenefit() / item.getWeight();
-//            ratios.add(ratio);
-//        }
+        Arrays.sort(items);
 
-        Item[] sortedItems = Arrays.copyOf(items, items.length);
-        Arrays.sort(sortedItems, Comparator.comparingDouble((Item item) -> (double)item.getBenefit() / item.getWeight()).reversed());
-
-        for (Item item : sortedItems) {
+        for (Item item : items) {
             if (totalLoad + item.getWeight() <= cap) {
                 totalLoad += item.getWeight();
-                totalBenefit += item.getBenefit();
-                combo.add(item);
+                bestBenefit += item.getBenefit();
+                bestCombo.add(item);
             } else {
                 break;
             }
         }
-        System.out.println("This is Knapsack number - " + knapsack.getId() + ".");
-        System.out.println("The knapsack's capacity is - " + cap + ".");
-        System.out.println("These are the following items in the knapsack : ");
-        for(int i = 0; i < items.length; i++) {
-            System.out.println("Item number " + (i + 1) + " has a benefit of " + items[i].getBenefit() + " and a weight of " + items[i] + ".");
-            System.out.printf("Resulting in a value(Benefit/Weight) of : %.3f", (double)items[i].getBenefit()/items[i].getWeight());
-        }
-        System.out.println("\nUsing the Greedy Algorithm resulted in the following items being added into the knapsack:");
-        for(Item item : combo) {
-            System.out.println(item);
-        }
-        System.out.println("For a total value of: " + totalBenefit);
+//        System.out.println("This is Knapsack number - " + knapsack.getId() + ".");
+//        System.out.println("The knapsack's capacity is - " + cap + ".");
+//        System.out.println("These are the following items in the knapsack : ");
+//        for(int i = 0; i < items.length; i++) {
+//            System.out.println("Item number " + (i + 1) + " has a benefit of " + items[i].getBenefit() + " and a weight of " + items[i] + ".");
+//            System.out.printf("Resulting in a value(Benefit/Weight) of : %.3f", (double)items[i].getBenefit()/items[i].getWeight());
+//        }
+//        System.out.println("\nUsing the Greedy Algorithm resulted in the following items being added into the knapsack:");
+//        for(Item item : combo) {
+//            System.out.println(item);
+//        }
+//        System.out.println("For a total value of: " + totalBenefit);
+    }
+
+    /**
+     * Method to return the best combination found
+     * @return Combination that doesn't exceed the Knapsack's capacity
+     */
+    public List<Item> getBestCombo() {
+        return bestCombo;
+    }
+
+    /**
+     * Method to return the best benefit found
+     * @return Total benefit from combination of Items that don't exceed the Knapsack's capacity
+     */
+    public int getBestBenefit() {
+        return bestBenefit;
     }
 }
