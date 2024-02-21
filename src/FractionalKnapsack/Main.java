9017d6498cbea837ad143a16a3db7083ade18bd2
Rory Hackney <roryhackney@gmail.com>
2024-02-20 16:59:12 -0800

    brute force continues

--- src/FractionalKnapsack/Main.java
+++ src/FractionalKnapsack/Main.java
@@ -7,17 +7,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BruteForce x = new BruteForce();
         Knapsack bag = new Knapsack(1, 5, new Item[]{new Item(3, 3), new Item(2, 2), new Item(1, 1)});
-        bag = KnapsackItems.FileReading.readFile("inputs5.csv");
+        bag = KnapsackItems.FileReading.readFile("inputs.csv");
+        BruteForce x = new BruteForce(bag);
         System.out.println(bag.getCapacity());
         System.out.println(Arrays.toString(bag.getItems()));
-        double res = x.solveTheProblem(bag);
-        System.out.println(res);
+        System.out.println(x.getMaxBenefit());
         System.out.println(x.getFinalItems());
 
         //Greedy gets different results
         System.out.println(Greedy.solveFractionalKnapsack(bag));
     }
-
 }
