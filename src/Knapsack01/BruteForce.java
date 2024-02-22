d80275d773679b407f566d80b9b103f781ee9698
Kelvin R <kelvinrbNsc@gmail.com>
2024-02-21 22:49:35 -0800

    Approach change to BF 01

--- src/Knapsack01/BruteForce.java
+++ src/Knapsack01/BruteForce.java
@@ -15,69 +15,111 @@ public class BruteForce {
     private List<Item> bestCombo;
     /** Private integer variable to keep track of the best possible benefits found **/
     private int bestBenefit;
+    /** Private integer variable to keep track of weight of the best possible combination found **/
+    private int bestWeight;
 
     /**
      * Constructor for Brute Force Algorithm
      * @param knapsack Desired Knapsack to run algorithm through
      */
     public BruteForce(Knapsack knapsack){
-        Item[] items = knapsack.getItems();
-        int cap = knapsack.getCapacity();
         bestCombo = new ArrayList<>();
+
+        Item[] items = knapsack.getItems();
         bestBenefit = 0;
+        bestWeight = 0;
+        int cap = knapsack.getCapacity();
+        int numOfItems = items.length;
+        int[] weights = new int[numOfItems];
+        int[] benefits = new int[numOfItems];
+        int[] bestChoice = new int[numOfItems];
+        int[] itemInKnapsack = new int[numOfItems];
 
-        generateCombos(items, cap, 0, new ArrayList<>());
-    }
+        for(int i = 0; i < numOfItems; i++) {
+            weights[i] = items[i].getWeight();
+            benefits[i] = items[i].getBenefit();
+        }
 
-    /**
-     * Method to generate all different possible combinations with all the Items available
-     * @param items Array of Item objects
-     * @param cap Capacity of the knapsack
-     * @param currentIndex Location of current Item in knapsack being inspected
-     * @param currentCombo A combination of items being put together
-     */
-    private void generateCombos(Item[] items, int cap, int currentIndex, List<Item> currentCombo) {
-        if (currentIndex == items.length) {
-            int currentWeight = calculateTotalWeight(currentCombo);
-            int currentBenefit = calculateTotalBenefit(currentCombo);
+        for (int i = 1; i <= Math.pow(2, numOfItems); i++) {
+            int j = numOfItems - 1;
+            int tempWeight = 0;
+            int tempBenefit = 0;
 
-            if (currentWeight <= cap && currentBenefit > bestBenefit) {
-                bestBenefit = currentBenefit;
-                bestCombo = new ArrayList<>(currentCombo);
+            while(itemInKnapsack[j] != 0 && j > 0) {
+                itemInKnapsack[j] = 0;
+                j--;
             }
-            return;
+            itemInKnapsack[j] = 1;
+            for (int k = 0; k < numOfItems; k++) {
+                if (itemInKnapsack[k] == 1) {
+                   tempBenefit += benefits[k];
+                   tempWeight += weights[k];
+                }
+            }
+            if ((tempBenefit > bestBenefit) && (tempWeight <= cap)){
+                bestBenefit = tempBenefit;
+                bestWeight = tempWeight;
+            }
+            bestChoice = itemInKnapsack;
         }
-        currentCombo.add(items[currentIndex]);
-        generateCombos(items, cap, currentIndex + 1, currentCombo);
-        currentCombo.remove(currentCombo.size() - 1);
-        generateCombos(items, cap, currentIndex + 1, currentCombo);
-    }
 
-    /**
-     * Method to calculate the total weight of a List of Items
-     * @param combo Combination of items to inspect
-     * @return Total weight of items on the List
-     */
-    private int calculateTotalWeight(List<Item> combo) {
-        int totalWeight = 0;
-        for(Item item : combo) {
-            totalWeight += item.getWeight();
+        for (int index = 0; index < bestChoice.length; index++) {
+            if (bestChoice[index] == 1) {
+                bestCombo.add(items[index]);
+            }
         }
-        return totalWeight;
+//        generateCombos(items, cap, 0, new ArrayList<>());
     }
-
-    /**
-     * Method to calculate the total benefit of a List of Items
-     * @param combo Combination of items to inspect
-     * @return Total benefit from items on the List
-     */
-    private int calculateTotalBenefit(List<Item> combo) {
-        int totalBenefit = 0;
-        for (Item item : combo) {
-            totalBenefit += item.getBenefit();
-        }
-        return totalBenefit;
-    }
+//
+//    /**
+//     * Method to generate all different possible combinations with all the Items available
+//     * @param items Array of Item objects
+//     * @param cap Capacity of the knapsack
+//     * @param currentIndex Location of current Item in knapsack being inspected
+//     * @param currentCombo A combination of items being put together
+//     */
+//    private void generateCombos(Item[] items, int cap, int currentIndex, List<Item> currentCombo) {
+//        if (currentIndex == items.length) {
+//            int currentWeight = calculateTotalWeight(currentCombo);
+//            int currentBenefit = calculateTotalBenefit(currentCombo);
+//
+//            if (currentWeight <= cap && currentBenefit > bestBenefit) {
+//                bestBenefit = currentBenefit;
+//                bestCombo = new ArrayList<>(currentCombo);
+//            }
+//            return;
+//        }
+//        currentCombo.add(items[currentIndex]);
+//        generateCombos(items, cap, currentIndex + 1, currentCombo);
+//        currentCombo.remove(currentCombo.size() - 1);
+//        generateCombos(items, cap, currentIndex + 1, currentCombo);
+//    }
+//
+//    /**
+//     * Method to calculate the total weight of a List of Items
+//     * @param combo Combination of items to inspect
+//     * @return Total weight of items on the List
+//     */
+//    private int calculateTotalWeight(List<Item> combo) {
+//        int totalWeight = 0;
+//        for(Item item : combo) {
+//            totalWeight += item.getWeight();
+//        }
+//        return totalWeight;
+//    }
+//
+//    /**
+//     * Method to calculate the total benefit of a List of Items
+//     * @param combo Combination of items to inspect
+//     * @return Total benefit from items on the List
+//     */
+//    private int calculateTotalBenefit(List<Item> combo) {
+//        int totalBenefit = 0;
+//        for (Item item : combo) {
+//            totalBenefit += item.getBenefit();
+//        }
+//        return totalBenefit;
+//    }
 
     /**
      * Method to return the best combination found
@@ -93,5 +135,13 @@ public class BruteForce {
      */
     public int getBestBenefit(){
         return bestBenefit;
+    }
+
+    /**
+     * Method to return the best total weight found
+     * @return Total weight from combination of Items that don't exceed the Knapsack's capacity
+     */
+    public int getBestWeight(){
+        return bestWeight;
     }
 }
